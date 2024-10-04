da4032481e4fdb70f41ae48b2ce76d752a4014c8
ST10158643 <ST10158643@vcconnect.edu.za>
2024-10-04 19:26:50 +0200

    Initial commit for dev-main branch

---
+++ Backend/check-auth.mjs
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+const checkauth=(req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token, "this_secret_should_be_longer_than_it_is");
+       // jwt.verify(token, process.env.JWT_KEY);
+       // req.userData = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            message: "Authentication failed, invalid token"
+        });
+    }
+};
+
+export default checkauth;
