22804c0906831c59d88ecb6072fd0542c1d0e5f1
ST10158643 <ST10158643@vcconnect.edu.za>
2024-11-07 11:11:22 +0200

    lint change for ci

--- Backend/eslint.config.js
+++ Backend/eslint.config.js
@@ -2,10 +2,18 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: { ...globals.browser, ...globals.node} }},
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+    settings: {
+      react: {
+        version: "detect", // or specify a specific version, e.g., "17.0"
+      },
+    },
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
